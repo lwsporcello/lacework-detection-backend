37b731a42627d589325fd327d95fee2b6b378357
Steve <79470244+lwsporcello@users.noreply.github.com>
2022-05-13 15:53:48 -0400

    Update api.py

--- api.py
+++ api.py
@@ -2,21 +2,35 @@
 # requires pip3 install flask
 # change the IP and port as necessary on last line
 
-from flask import Flask, json, request
+from flask import Flask, json, request, send_file, abort
+import logging
+import os
 
+#define flask api
 api = Flask(__name__)
 
-my_json = [{"id":1, "name":"my_name"}]
+#define constants
+BIN_DIR = "/home/ubuntu/se-det-demo/bin"
 
-@api.route('/api', methods=['GET'])
-def get_json():
-  return json.dumps(my_json)
+#set up logging
+handler = logging.FileHandler("/var/log/api.log")
+log = logging.getLogger('werkzeug')
+log.addHandler(handler)
+log.setLevel(logging.INFO)
 
-@api.route('/api', methods=['POST'])
+#define endpoints
+@api.route('/bin/<path:file>', methods=['GET'])
+def download_file(file):
+	try:
+		return send_file(BIN_DIR+"/"+file, download_name=file), 200
+	except:
+		abort(404)
+
+@api.route('/lw-beacon', methods=['POST'])
 def post_json():
   data_json = request.json
-  print(data_json)
+  log.info(data_json)
   return json.dumps({"Success":True}), 200
 
 if __name__ == '__main__':
-    api.run(host="0.0.0.0", port=8888)
+    api.run(host="0.0.0.0", port=8002)
